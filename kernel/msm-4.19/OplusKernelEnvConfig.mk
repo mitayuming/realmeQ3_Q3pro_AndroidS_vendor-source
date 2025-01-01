93f1ae33c1760b7e4c5525178c749f9d8fd648d7
mitayuming <88870511+mitayuming@users.noreply.github.com>
2025-01-02 00:53:14 +0800

    Update OplusKernelEnvConfig.mk

--- kernel/msm-4.19/OplusKernelEnvConfig.mk
+++ kernel/msm-4.19/OplusKernelEnvConfig.mk
@@ -135,7 +135,7 @@ endif
 
 
 $(foreach myfeature,$(ALLOWED_MCROS),\
-         $(warning make $(myfeature) to be a macro here) \
+         $(#warning make $(myfeature) to be a macro here) \
          $(eval KBUILD_CFLAGS += -D$(myfeature)) \
          $(eval KBUILD_CPPFLAGS += -D$(myfeature)) \
          $(eval CFLAGS_KERNEL += -D$(myfeature)) \
